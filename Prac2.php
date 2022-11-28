0fd2639f30b93f6a70bdf0beeb47d759d8c82b85
NobuSora <119284433+NobuSora@users.noreply.github.com>
2022-11-28 17:02:59 +0800

    Add files via upload

---
+++ Prac2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+    <form method="post">
+        Enter Value to Loop:
+    <input type="number" name="value" value="" placeholder="Enter value"/>
+    <input type="submit" name="submit" value="Submit"/>
+    </form>
+    <br>
+    <?php
+        if(isset($_POST['submit'])) {
+            function test($val) {
+                //Printing Spaces
+                for ($i = 0; $i <= $val; $i++) {
+                    
+                    for ($j=0; $j < $val - $i; $j++) { 
+                        echo "&nbsp;&nbsp;";
+                    }
+                    //Print Stars
+                    for ($x=0; $x <= $i; $x++) { 
+                     echo "*";
+                    }
+                    echo"<br>";
+                }
+            }
+        $val = $_POST['value'];
+        test($val);
+        }
+        else{
+            echo "No Variable is submitted";
+        }
+    ?>
+    </body>
+</html>
