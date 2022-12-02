ac48302160dbea87335cc39221b87d4519fa31e6
NobuSora <119284433+NobuSora@users.noreply.github.com>
2022-12-02 16:32:08 +0800

    Add files via upload

--- Training/jQuery/Activity1/js/main.js
+++ Training/jQuery/Activity1/js/main.js
@@ -1,43 +1,58 @@
 $(document).ready(function()
 {
-    //Animations
-    $("#inInc").hover(
-		//Hover In
-        function () {
-           $(this).css({"background-color":"green"});
-        }, 
-        //Hover Out
-        function () {
-           $(this).css({"background-color":"white"});
-        }
-    )
-    $("#inDec").hover(
-		//Hover In
-        function () {
-           $(this).css({"background-color":"red"});
-        }, 
-        //Hover Out
-        function () {
-           $(this).css({"background-color":"white"});
-        }
-    )
     //Operations---------------------------------------------------------
     ////Increase Value
+    var ResultVal;
     $("#inInc").click(function()
     {
-        var CurVal = $("#valOut").text();
-        ResultVal = parseInt(CurVal, 10) + 1; //Parsing and Computation
+        //Animation
+        $(this).animate({
+            boxShadow: "0px 0px 0px 0px rgb(0, 0, 0)",
+            backgroundColor: "green",
+            height: "45px",
+            width: "45px"
+        },50);
+        $(this).animate({
+            boxShadow: "0px 0px 5px 2px rgb(0, 0, 0)",
+            backgroundColor: "white",
+            height: "50px",
+            width: "50px"
+        },50);
+        //Process
+        var CurVal = parseInt($("#valOut").text(), 10); //Parsing
+        ResultVal = CurVal + 1;
         $("#valOut").text(ResultVal);
-        $(this).css({"background-color":"green"});
     });
+
     ////Decrease Value
     $("#inDec").click(function()
     {
-        var state = true;
-        var CurVal = $("#valOut").text();
-        ResultVal = parseInt(CurVal, 10) - 1; //Parsing and Computation
-        $("#valOut").text(ResultVal);
-        $(this).css({"background-color":"red"});
+
+        var CurVal = parseInt($("#valOut").text(), 10); //Parsing
+        if(CurVal=== 0){
+            
+            //Process
+            ResultVal = CurVal;
+            alert("Nope...We don't do that here.");
+        }else
+        {
+            //Animation
+            $(this).animate({
+                boxShadow: "0px 0px 0px 0px rgb(0, 0, 0)",
+                backgroundColor: "red",
+                height: "45px",
+                width: "45px"
+            },50);
+            $(this).animate({
+                boxShadow: "0px 0px 5px 2px rgb(0, 0, 0)",
+                backgroundColor: "white",
+                height: "50px",
+                width: "50px"
+            },50);
+            ResultVal = CurVal - 1;
+            $("#valOut").text(ResultVal);
+        }
+        
     });
     //Operations-End-----------------------------------------------------
 });
